c7ecc689e8635d2cc0ca312c7e5160daa883020e
Firdavs Beknazarov <opendeeple@gmail.com>
2025-07-16 14:14:29 +0500

    fix: cors

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -1,8 +1,14 @@
 import fp from "fastify-plugin";
 import cors from "@fastify/cors";
+import { FastifyPluginAsync } from "fastify";
 
-export default fp(async (fastify) => {
-  fastify.register(cors, {
-    origin: true,
+export default fp<FastifyPluginAsync>(async (fastify) => {
+  await fastify.register(cors, {
+    origin: (origin, cb) => {
+      console.log("Cors origin:", origin);
+      cb(null, true);
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   });
 });
